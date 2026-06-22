2e04c2456db1dc211782d9505e990e2b792d1cb8
Abdulmujib Oladayo <aderonkeadejare54@gmail.com>
2026-06-22 09:53:03 +0100

    feat: add NoPenaltiesToWithdraw error variant

--- contracts/reward/src/errors.rs
+++ contracts/reward/src/errors.rs
@@ -2,17 +2,9 @@ use soroban_sdk::contracterror;
 
 #[contracterror]
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(u32)]
 pub enum Error {
     Unauthorized = 1,
     InvalidSignature = 2,
-}
-
-use soroban_sdk::contracttype;
-
-#[derive(Clone)]
-#[contracttype]
-pub enum RewardError {
-    SignatureExpired = 1,
-    NonceAlreadyUsed = 2,
-    InvalidSignature = 3,
+    NoPenaltiesToWithdraw = 3,
 }
